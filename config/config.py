76bba77949031225e34b2c0e9325b5ef6e07ddf6
Ling Zi Jian <lingzijian@hotmail.com>
2026-03-25 10:30:23 +0800

    feat: config.py

---
+++ config/config.py
@@ -0,0 +1,9 @@
+# Forum base URL
+BASE_FORUM_URL = "https://royals.ms/forum/"
+
+# Scraping defaults
+DEFAULT_MAX_PAGES = 2
+
+# Thread pool workers
+PAGE_FETCH_WORKERS = 5
+THREAD_FETCH_WORKERS = 10
