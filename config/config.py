7a82a0c9f586c3e9fffebec0f1b517d7267d4908
Ling Zi Jian <lingzijian@hotmail.com>
2026-03-26 10:53:22 +0800

    feat: config settings for filtering lists

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 # Forum base URL
 BASE_FORUM_URL = "https://royals.ms/forum/"
+SELLING_FORUM_URL = "https://royals.ms/forum/forums/selling.17/"
 
 # Scraping defaults
 DEFAULT_MAX_PAGES = 2
@@ -7,3 +8,7 @@ DEFAULT_MAX_PAGES = 2
 # Thread pool workers
 PAGE_FETCH_WORKERS = 5
 THREAD_FETCH_WORKERS = 10
+
+# Cache settings
+CACHE_FILE_PATH = "cache/listings.csv"
+CACHE_TTL_MINUTES = 30
